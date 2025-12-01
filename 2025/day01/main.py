6ea3e44bb0e4146b6013168074f77a78a45c2509
Paradoxisme Dev <contact@paradoxisme.fr>
2025-12-01 18:27:18 +0100

    part 1 done

---
+++ 2025/day01/main.py
@@ -0,0 +1,15 @@
+start_position = 50
+
+current_position = start_position
+number_pointing_zero = 0
+with open("input.txt", 'r') as f:
+    for line in f:
+        if line[0] == 'R':
+            current_position += int(line[1:])
+        else:
+            current_position -= int(line[1:])
+        current_position %= 100
+        if current_position == 0:
+            number_pointing_zero += 1
+
+print(number_pointing_zero)
