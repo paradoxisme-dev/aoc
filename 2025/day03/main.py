14e97d7e83c9c7db76bd7b9f02c2d018b5614a0e
Paradoxisme Dev <contact@paradoxisme.fr>
2025-12-03 18:56:43 +0100

    fin jour 3 partie 1

--- 2025/day03/main.py
+++ 2025/day03/main.py
@@ -1,1 +1,21 @@
-print("Je fais un thè et j'arrive.")
+total_joltage = 0
+
+with open('input.txt', 'r') as file:
+    for line in file:
+        for current_value in ["9", "8", "7", "6", "5", "4", "3", "2", "1"]:
+            try:
+                index_value = line[:len(line)-2].index(current_value)
+                string_value = current_value
+                break
+            except:
+                pass
+        for current_value in ["9", "8", "7", "6", "5", "4", "3", "2", "1"]:
+            try:
+                line[index_value+1:].index(current_value)
+                string_value += current_value
+                break
+            except:
+                pass
+        total_joltage += int(string_value)
+
+print(total_joltage)
