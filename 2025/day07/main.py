51f80974b634792f660f577413c79d83be60b6f1
Paradoxisme Dev <contact@paradoxisme.fr>
2025-12-07 18:42:20 +0100

    fin partie 1 jour 7

--- 2025/day07/main.py
+++ 2025/day07/main.py
@@ -1,1 +1,25 @@
-print("Je prépare mon thé et j'arrive.")
+number_split = 0
+with open('input.txt', 'r') as file:
+    for line in file:
+        line = line[:-1]
+        if line.count('S') != 0:
+            beams = list(line.replace('S', '|'))
+        if line.count('^') != 0:
+            new_beam = beams
+            while True:
+                try:
+                    index_spliter = line.index('^')
+                    if beams[index_spliter] == '|':
+                        new_beam[index_spliter] = '.'
+                        if index_spliter - 1 > 0:
+                            new_beam[index_spliter-1] = '|'
+                        if index_spliter + 1 < len(new_beam):
+                            new_beam[index_spliter + 1] = '|'
+                        number_split += 1
+                    line = line[:index_spliter] + '.' + line[index_spliter+1:]
+                except:
+                    break
+            beams = new_beam
+
+
+print(number_split)
