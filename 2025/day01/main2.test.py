c03bce2f43fc7d69508a2e53522d5bbfa6935123
Paradoxisme Dev <contact@paradoxisme.fr>
2026-01-25 17:19:35 +0100

    correction test

--- 2025/day01/main2.test.py
+++ 2025/day01/main2.test.py
@@ -19,8 +19,11 @@ class Rotor:
 rotor = Rotor()
 current_position = 50
 number_zero = 0
+delta_value = 0
 with open("input.txt", 'r') as f:
+    old_zero = False
     for line_nb, line in enumerate(f):
+        old_value = current_position
         displacement_value = int(line[1:])
         if line[0] == 'R':
             current_position += displacement_value
@@ -32,18 +35,35 @@ with open("input.txt", 'r') as f:
             current_position -= displacement_value
         old_number_zero = number_zero
         if current_position < 0:
-            number_zero += abs(current_position // 100)-1
+            number_zero += abs(current_position // 100)
+            if old_zero:
+                number_zero -= 1
+            old_zero = False
+            if current_position % 100 == 0:
+                number_zero += 1
+                old_zero = True
+        elif current_position % 100 == 0:
+            old_zero = True
+            if current_position > 100:
+                number_zero += current_position // 100
+            else:
+                number_zero += 1
         else:
             number_zero += current_position // 100
+            old_zero = False
         current_position %= 100
-        if current_position == 0:
-            number_zero += 1
         if number_zero != rotor.zero_count:
             print('old_number_zero', old_number_zero)
-            print(current_position)
+            print('delta_value', delta_value)
+            print('old_value', old_value)
             print('current_position : ', current_position)
             print('rotor.current : ', rotor.current)
             print('line : ', line[:-1], line_nb)
-            print('number_zero != rotor.zero_count', number_zero, rotor.zero_count)            
+            print('number_zero != rotor.zero_count', number_zero, rotor.zero_count)
+            break
+        else:
+            if old_number_zero != number_zero:
+                print('number_zero :', number_zero)
+                print('line : ', line_nb)
 
 print(rotor.zero_count)
